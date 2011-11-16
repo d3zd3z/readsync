3fe8deb5bb4e2c8f2493ccb699943d6423b4956b
David Brown <davidb@codeaurora.org>
2011-11-16 14:51:53 -0800

    Prompt for connection information
    Rather than passing a password on the command line, prompt the user
    for this information.

--- schema.sh
+++ schema.sh
@@ -2,16 +2,34 @@
 
 DB=rs-state.db
 
+readsecret()
+{
+	echo -n "$1: " >&2
+	oldmode=`stty -g`
+	stty -echo
+	read pass
+	stty "$oldmode"
+	echo '' >&2
+	echo $pass
+}
+
+readnonsecret()
+{
+	echo -n "$1: " >&2
+	read text
+	echo $text
+}
+
 if [ -f "$DB" ]; then
 	sqlite3 "$DB" '.dump server' > /tmp/server$$
 	rm "$DB"
 	sqlite3 "$DB" < /tmp/server$$
 	rm /tmp/server$$
 else
-	# Pass these in.
-	server="$1"
-	user="$2"
-	pass="$3"
+	# Prompt the user for this information.
+	server=`readnonsecret server host`
+	user=`readnonsecret user`
+	pass=`readsecret password`
 
 	sqlite3 "$DB" <<Z
 create table server (user text, pass text, host text);
