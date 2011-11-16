1dba199ec92f6237a08e5cfa821608e875dc7960
David Brown <davidb@codeaurora.org>
2011-11-16 13:43:42 -0800

    Mark table rows as being 'not null'
    Catches some errors when inserting rows.

--- schema.sh
+++ schema.sh
@@ -5,7 +5,7 @@ server="$1"
 user="$2"
 pass="$3"
 
-folders='CAF CAF.linaro-dev CAF.linux-arm-kernel CAF.linux-arm-msm CAF.linux-kernel CAF.devicetree CAF.linux-next'
+folders='CAF CAF.linaro-dev CAF.linux-arm-kernel CAF.linux-arm-msm CAF.linux-kernel CAF.devicetree CAF.linux-next CAF.rtc-linux CAF.git'
 sfolder=''
 for i in $folders; do
 	sfolder="$sfolder
@@ -19,17 +19,17 @@ create table server (user text, pass text, host text);
 insert into server values('$user', '$pass', '$server');
 create table folders (
    key integer primary key autoincrement,
-   name text,
-   validity int64
+   name text not null,
+   validity int64 not null
 );
 $sfolder
 
 create table idmap (
-   folderKey integer references folders(key),
-   validity int64,
-   uid int64,
-   messageid blob,
-   seen boolean);
+   folderKey integer references folders(key) not null,
+   validity int64 not null,
+   uid int64 not null,
+   messageid blob not null,
+   seen boolean not null);
 create index idmap_mid on idmap (messageid);
 create index idmap_id on idmap (validity, uid);
 Z
